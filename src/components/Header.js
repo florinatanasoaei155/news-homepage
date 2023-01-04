609bf60734e6314933063e5adc3455aae2da3666
Florin Atănăsoaei <atanasoaeiflorin@gmail.com>
2023-01-04 08:58:49 +0200

    Improve header style on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
 
   return (
     <header className="header">
-      <a href="/">
+      <a href="/" className="logo-wrapper">
         <img src={Logo} alt="Logo" />
       </a>
       <button onClick={toggleSidebar} className="icon-button">
